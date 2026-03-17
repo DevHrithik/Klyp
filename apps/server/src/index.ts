d27f3e23d344feaa74f83f4a831a91a53b3893c3
Hrithik Roy <hrithikroy1854@gmail.com>
2026-03-18 00:30:19 +0530

    chore: Swap server and web app development ports and update web app tsconfig.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -66,6 +66,6 @@ app.get("/", (_req, res) => {
 	res.status(200).send("OK");
 });
 
-app.listen(3000, () => {
-	console.log("Server is running on http://localhost:3000");
+app.listen(3001, () => {
+	console.log("Server is running on http://localhost:3001");
 });
